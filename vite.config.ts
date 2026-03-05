43dc507d19081e48b389a644fa6386ffa6d2c721
regginssa <randyarabis428@gmail.com>
2026-03-06 03:12:40 +0900

    feat(vite.config.ts): Add build config to export static page when it's being built

--- vite.config.ts
+++ vite.config.ts
@@ -12,10 +12,15 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [react(), mode === "development" && componentTagger()].filter(
+    Boolean,
+  ),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  build: {
+    outDir: "out",
   },
 }));
